f179911a5f6ac7c12939d08ca84cb96c9c7b0d11
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-07-17 20:28:57 +0300

    shed sentence complete

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,15 @@ function produceReport(){
 
     data[0] = data["Shed A"] = perseInt(document.getElementById("Shed A").value);
     data[1] = data["Shed B"] = perseInt(document.getElementById("Shed B").value);
-    data[2] = data["Shed C"] =parseInt(document.getElementById("Shed C").value);
-    data[3] = data["Shed D"] =perseInt(document.getElementById("Shed D").value);
+    data[2] = data["Shed C"] = parseInt(document.getElementById("Shed C").value);
+    data[3] = data["Shed D"] = perseInt(document.getElementById("Shed D").value);
+
+    let sumData = data.Shed A + data.Shed B + data.Shed C + data.Shed D;
+
+    document.getElementById("dataOutput").innerHTML += "<Br>"
+    document.getElementById("dataOutput").innerHTML += "<P> Youy production in Shed A is" + data.Shed A + "Litres Per Day <P>"
+    document.getElementById("dataOutput").innerHTML += "<P> Youy production in Shed B is" + data.Shed B + "Litres Per Day <P>"
+    document.getElementById("dataOutput").innerHTML += "<P> Youy production in Shed C is" + data.Shed C + "Litres Per Day <P>"
+    document.getElementById("dataOutput").innerHTML += "<P> Youy production in Shed D is" + data.Shed D + "Litres Per Day <P>"
+
 }
