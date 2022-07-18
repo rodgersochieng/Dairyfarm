e8e95caa8a6f0d9952d197741f83a66ab37e6905
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-07-18 22:54:17 +0300

    finalcommit

--- assets/index.js
+++ assets/index.js
@@ -1,38 +1,45 @@
-function produceReport(){
+function produceReport() {
 
     event.preventDefault();
-    let data =[];
 
-    data[0] = data["Shed A"] = perseInt(document.getElementById("Shed_A").value);
-    data[1] = data["Shed B"] = perseInt(document.getElementById("Shed_B").value);
-    data[2] = data["Shed C"] = parseInt(document.getElementById("Shed_C").value);
-    data[3] = data["Shed D"] = perseInt(document.getElementById("Shed_D").value);
+    let data = [];
 
-    let sumData = data.Shed_A + data.Shed_B + data.Shed_C + data.Shed_D;
+     data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+     data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+     data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+     data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
-    document.getElementById("dataOutput").innerHTML += "<Br>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your production in Shed A is" + data.Shed_A + "Litres Per Day </p>";
-    document.getElementById("dataOutput").innerHTML += "<p> Your production in Shed B is" + data.Shed_B + "Litres Per Day </p>";
-    document.getElementById("dataOutput").innerHTML += "<p> Your production in Shed C is" + data.Shed_C + "Litres Per Day </p>";
-    document.getElementById("dataOutput").innerHTML += "<p> Your production in Shed D is" + data.Shed_D + "Litres Per Day </p>";
-     
+    let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 
-    document.getElementById("dataOutput").innerHTML += "<p> Your total production per day is" + sumData + "Litres per day </p>";
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
+
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
+    
 
     dailyIncome = sumData * 45;
-    document.getElementById("dataOutput").innerHTML += "<Br>"
 
-    document.getElementById("dataOutput").innerHTML += "<hr>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total Daily income is Ksh. "+ dailyIncome + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<hr>"
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    document.getElementById("outputData").innerHTML += "<hr>"
+
+    document.getElementById("outputData").innerHTML += "<p>Your total daily income is Ksh. " + dailyIncome + " </p>";
+
+    document.getElementById("outputData").innerHTML += "<hr>"
 
     weeklyIncome = dailyIncome * 7;
 
-    document.getElementById("dataOutput").innerHTML += "<p>Your total weekly income is Ksh. " + weeklyIncome + " </p>";
+    document.getElementById("outputData").innerHTML += "<p>Your total weekly income is Ksh. " + weeklyIncome + " </p>";
 
-    document.getElementById("dataOutput").innerHTML += "<hr>"
+    document.getElementById("outputData").innerHTML += "<hr>"
 
-    document.getElementById("dataOutput").innerHTML += "<Br>"
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+
 
     incomeJanuary = dailyIncome * 31;
     incomeFebruary = dailyIncome * 29;
@@ -47,42 +54,72 @@ function produceReport(){
     incomeNovember = dailyIncome * 30;
     incomeDecember = dailyIncome * 31;
 
-
-    
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for January is Ksh. " + incomeJanuary + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for February is Ksh. " + incomeFebruary + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for March is Ksh. " + incomeMarch + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for April is Ksh. " + incomeApril + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for May is Ksh. " + incomeMay + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for June is Ksh. " + incomeJune + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for July is Ksh. " + incomeJuly + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for August is Ksh. " + incomeAugust + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for September is Ksh. " + incomeSeptember + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for October is Ksh. " + incomeOctober + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for November is Ksh. " + incomeNovember + " </p>"
-    document.getElementById("dataOutput").innerHTML += "<p> Your total income for December is Ksh. " + incomeDecember + " </p>"
-
+     
+    document.getElementById("outputData").innerHTML += "<p> Your total income for January is Ksh. " + incomeJanuary + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for February is Ksh. " + incomeFebruary + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for March is Ksh. " + incomeMarch + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for April is Ksh. " + incomeApril + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for May is Ksh. " + incomeMay + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for June is Ksh. " + incomeJune + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for July is Ksh. " + incomeJuly + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for August is Ksh. " + incomeAugust + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for September is Ksh. " + incomeSeptember + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for October is Ksh. " + incomeOctober + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for November is Ksh. " + incomeNovember + " </p>"
+    document.getElementById("outputData").innerHTML += "<p> Your total income for December is Ksh. " + incomeDecember + " </p>"
 
 
+    document.getElementById("outputData").innerHTML += "<Br>"
 
-    document.getElementById("dataOutput").innerHTML += "<Br>"
-
-    document.getElementById("dataOutput").innerHTML += "<hr>"
+    document.getElementById("outputData").innerHTML += "<hr>"
 
     incomeInLeapYear = dailyIncome * 366;
 
-    document.getElementById("dataOutput").innerHTML += "<p>Your total income in a leap year is Ksh. " + incomeInLeapYear + " </p>";
+    document.getElementById("outputData").innerHTML += "<p>Your total income in a leap year is Ksh. " + incomeInLeapYear + " </p>";
 
-    document.getElementById("dataOutput").innerHTML += "<hr>"
+    document.getElementById("outputData").innerHTML += "<hr>"
 
-    document.getElementById("dataOutput").innerHTML += "<Br>"
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    newDailyIncome = sumData * 49.60;
+
+    newIncomeJanuary = newDailyIncome * 31;
+    newIncomeFebruary = newDailyIncome * 29;
+    newIncomeMarch = newDailyIncome * 31;
+    newIncomeApril = newDailyIncome * 30;
+    newIncomeMay = newDailyIncome * 31;
+    newIncomeJune = newDailyIncome * 30;
+    newIncomeJuly = newDailyIncome * 31;
+    newIncomeAugust = newDailyIncome * 31;
+    newIncomeSeptember = newDailyIncome * 30;
+    newIncomeOctober = newDailyIncome * 31;
+    newIncomeNovember = newDailyIncome * 30;
+    newIncomeDecember = newDailyIncome * 31;
+
+    newIncomeInLeapYear = 366 * 49.60;
+
+    incomeJanuaryDifference = newIncomeJanuary - incomeJanuary;
+    incomeFebruaryDifference = newIncomeFebruary - incomeFebruary;
+    incomeMarchDifference = newIncomeMarch - incomeMarch;
+    incomeAprilDifference = newIncomeApril - incomeApril;
+    incomeMayDifference = newIncomeMay - incomeMay;
+    incomeJuneDifference = newIncomeJune - incomeJune;
+    incomeJulyDifference = newIncomeJuly - incomeJuly;
+    incomeAugustDifference = newIncomeAugust - incomeAugust;
+    incomeSeptemberDifference = newIncomeSeptember - incomeSeptember;
+    incomeOctoberDifference = newIncomeOctober - incomeOctober;
+    incomeNovemberDifference = newIncomeNovember - incomeNovember;
+    incomeDecemberDifference = newIncomeDecember - incomeDecember;
+
+
+
+    function resetPage(){
+
+        document.getElementById("outputData").remove("innerHTML");
+        location.reload();
+    }
 
 
 
 
-
-
-
-
-
-} 
+    }
